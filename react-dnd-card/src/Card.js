ebc4067ac22c54b95f1b87473763e124f9c2c939
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-06-24 23:51:50 +0900

    Updated

--- react-dnd-card/src/Card.js
+++ react-dnd-card/src/Card.js
@@ -1,8 +1,29 @@
-import React, { useState, useCallback } from 'react';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import { ItemTypes } from './ItemTypes';
+
+const style = {
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  marginBottom: '.5rem',
+  backgroundColor: 'white',
+  cursor: 'move'
+}
 
 const Card = ({ id, text, index, moveCard }) => {
+  console.log(moveCard);
+
+  const ref = useRef(null);
+
+  const [{ isDragging }, drag] = useDrag({
+    item:    { type: ItemTypes.CARD, id, index },
+    collect: (monitor) => ({ isDragging: monitor.isDragging() })
+  });
+
+  const opacity = isDragging ? 0 : 1;
+
   return (
-    <div>aaaaa</div>
+    <div>id={id}, text={text}, index={index}, moveCard=</div>
   )
 }
 
